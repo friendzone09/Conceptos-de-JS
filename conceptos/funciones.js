a02cc865b82624a91af0f4710c2cb09b51855bd5
Alejandro D Leon <alejandroperez9474@gmail.com>
2024-09-17 14:02:05 -0600

    version 1.2

---
+++ conceptos/funciones.js
@@ -0,0 +1,32 @@
+// function nameFunction(params){
+//     body
+// }
+
+//Arrow function
+// const nameFunction2 = (params) =>{
+//     body 
+//     return
+// }
+
+function sayHello(){
+    console.log('Hello');
+};
+
+sayHello();
+
+console.log('========================================================================================');
+
+const sayHelloInSpanish = () => console.log('Hola');
+sayHelloInSpanish();
+
+console.log('========================================================================================');
+
+function sayHelloto(name){
+    console.log(`Hello ${ name}`);
+}
+
+sayHelloto('Yayo');
+
+const sayHelloTo1 = (name) => console.log(`Hello ${ name}`);
+sayHelloTo1('Momo');
+
